446ef91bb842a15acb5e8bcb6f011fd3322aeeb0
HenedioNeto <netopedrosa13@gmail.com>
2023-08-25 14:02:29 -0300

    Uso do while e fstring

--- basico01/f_controle_fluxo2.py
+++ basico01/f_controle_fluxo2.py
@@ -10,8 +10,16 @@ def construir_url():
         contador = contador - 30
         print(url_completa)
 
+def url_teste():
+    contador = 0
+    while contador <= 90:
+        link_teste = f"https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa?b_start:int={contador}"
+        contador += 30
+        print(link_teste)
+
 def main():
-    construir_url()
+    #construir_url()
+    url_teste()
 
 if __name__ == "__main__":
     main()
