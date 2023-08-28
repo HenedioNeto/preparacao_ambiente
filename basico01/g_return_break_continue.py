a54eb5de0d26521716596b81987bb3661d37c626
HenedioNeto <netopedrosa13@gmail.com>
2023-08-28 18:57:48 -0300

    Atualizações no arquivo

--- basico01/g_return_break_continue.py
+++ basico01/g_return_break_continue.py
@@ -18,16 +18,15 @@ def construir_url():
             url_completa = "https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa"
         contador = contador - 30
         list_url.append(url_completa)
-        return list_url
-        print(list_url)
+    return list_url
+
+print(construir_url())
 
 def main():
     url = "https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa"
     #pagina_web = acessar_pagina(url)
-    lista_urls = construir_url()
+    construir_url()
     
 
 if __name__ == "__main__":
     main()
-
-["https...", "https..."]
